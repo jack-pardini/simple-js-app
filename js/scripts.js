7c3e83c86e887582d129d07dd638f6c914b65f2a
jack-pardini <jack.pardini@gmail.com>
2024-02-16 10:47:19 -0800

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+alert('Hello world');
+
+let myName = 'Jack';
+document.write(myName);
+
+myName = 'John Doe';
+document.write(myName);
+
+let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+
+let simpleSubtraction = 10 - 5;
+document.write(simpleSubtraction);
+
+let size = 100;
+let doubleSize = size * 2;
+document.write(doubleSize);
+
+let halfSize = size / 2;
+document.write(halfSize);
+
+let minSize = (doubleSize * 2) - (size /2);
+document.write(minSize);
