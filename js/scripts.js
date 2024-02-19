65f7ea4c73a3388d696c89b2cfccfff516cff748
jack-pardini <jack.pardini@gmail.com>
2024-02-19 14:54:54 -0800

    changed <br> to <p>

--- js/scripts.js
+++ js/scripts.js
@@ -62,10 +62,11 @@ let pokemon4 =
   type: 'Electric'
 }
 
+
 for (let i = 0; i < pokemons.length; i++) {
-    if (pokemons[i].height > 1.9) {
-      document.write(pokemons[i].name + ' (height: ' + (pokemons[i].height) + 'm) - Wow, that\'s big! <br><br>');
-    } else if (pokemons[i].height < 1.9 ){
-        document.write(pokemons[i].name + ' (height: ' + (pokemons[i].height) + 'm)<br><br>');
-  }
+  if (pokemons[i].height > 1.9) {
+    document.write('<p>' + pokemons[i].name + ' (height: ' + (pokemons[i].height) + 'm) - Wow, that\'s big!</p>');
+  } else if (pokemons[i].height < 1.9 ){
+      document.write('<p>' + pokemons[i].name + ' (height: ' + (pokemons[i].height) + 'm)</p>');
+}
 }
